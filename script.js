5c373edc68924706a593413c323b4ead5af11dd4
alefo120592 <alefo120592@gmail.com>
2025-09-02 13:23:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const PRODUCTS = [
   { id: 'p1', nombre: 'Audífonos inalámbricos Airpods', precio: 60000, images: ['assets/img/Audífonos inalámbricos Airpods/Audífonos inalámbricos Airpods.webp','assets/img/Audífonos inalámbricos Airpods/Audífonos inalámbricos Airpods 1.webp','assets/img/Audífonos inalámbricos Airpods/Audífonos inalámbricos Airpods 2.webp','assets/img/Audífonos inalámbricos Airpods/Audífonos inalámbricos Airpods 3.webp'], descripcion: 'Audífonos de alta calidad para una experiencia de audio inmersiva.' },
   { id: 'p2', nombre: 'Combo audifonos + smartwatch Q9', precio: 80000, images: ['assets/img/Combo/Combo 1.jpeg', 'assets/img/Combo/Combo 2.png'], descripcion: 'El combo perfecto para tu día a día, con audífonos y smartwatch.' },
   { id: 'p3', nombre: 'Pilas Beston Recargable X4 Con Cargador', precio: 40000, images: ['assets/img/Betson/betson 1.webp', 'assets/img/Betson/betson 2.webp'], descripcion: 'No te quedes sin energía. La duración está garantizada.' },
+  { id: 'p4', nombre: 'Android TV Stick!', precio: 80000, images: ['assets/img/Tv Stick/Tvstick','assets/img/Tv Stick/Tvstick1','assets/img/Tv Stick/Tvstick2'], descripcion: '¡No esperes más para elevar tu experiencia de entretenimiento!' },
+  { id: 'p5', nombre: 'Pistola de hidrogel G36', precio: 110000, images: ['assets/img/Pistohidrogel/Phidrogel','assets/img/Pistohidrogel/Phidrogel1','assets/img/Pistohidrogel/Phidrogel2'], descripcion: '¡Aventuras épicas y horas de diversión garantizadas! Con esta pistola de juguete, la imaginación no tendrá límites.' },
  
 
 ];
@@ -275,3 +277,4 @@ renderProducts();
 renderCart();
 
 updateCartCount();
+
